829c430385e75cb93c5da3caa1493d76cda18f58
cuong11 <cuongtran11052000@gmail.com>
2021-11-13 05:28:50 +0700

    Load data music

--- asset/run.js
+++ asset/run.js
@@ -23,59 +23,59 @@ const App = {
     songs: [{
             name: "Easy On Me",
             singer: "Adele",
-            path: "https://data25.chiasenhac.com/download2/2202/5/2201284-0ac3f306/128/Easy%20On%20Me%20-%20Adele%20(NhacPro.net).mp3",
+            path: "./asset/music/EasyOnMe.mp3",
             image: "https://i2.wp.com/medianub.com/wp-content/uploads/2021/11/Adele-Easy-On-Me.jpeg?fit=695%2C441&ssl=1"
         }, {
             name: "East Of Eden",
             singer: "Zella Day",
-            path: "https://data04.chiasenhac.com/downloads/1508/5/1507159-b2a8caa8/128/East%20Of%20Eden%20-%20Zella%20Day.mp3",
+            path: "./asset/music/EastOfEden.mp3",
             image: "https://data.chiasenhac.com/data/cover/43/42035.jpg"
         }, {
             name: "Walk Thru Fire",
             singer: "Vicetone ft Meron Ryan",
-            path: "https://data34.chiasenhac.com/downloads/1939/5/1938435-5007db99/128/Walk%20Thru%20Fire%20-%20Vicetone_%20Meron%20Ryan.mp3",
+            path: "./asset/music/WalkThruFire.mp3",
             image: "https://avatar-ex-swe.nixcdn.com/song/2018/08/06/f/5/6/1/1533518669570_640.jpg"
         }, {
             name: 'See You Again',
             singer: 'Charlie Puth ft Wiz Khalifa',
-            path: 'https://aredir.nixcdn.com/NhacCuaTui894/SeeYouAgain-KurtSchneiderEppicAlexGoot-3888930.mp3?st=1q73myBS8FKr8Rx0snpMJw&e=1623144094',
+            path: './asset/music/SeeYouAgain.mp3',
             image: 'https://nghiennhac.com/wp-content/uploads/2020/09/see-you-again-0.jpg',
         },
         {
             name: "Unstoppable",
             singer: "Sia",
-            path: "https://data2.chiasenhac.com/stream2/1620/5/1619524-39627886/128/Unstoppable%20-%20Sia.mp3",
+            path: "./asset/music/Unstoppable.mp3",
             image: "https://mp3how.com/wp-content/uploads/2021/08/download-2.jpeg"
         }, {
             name: "Halo",
             singer: "Beyonce",
-            path: "https://data51.chiasenhac.com/downloads/1007/5/1006846-2972b850/128/Halo%20-%20Beyonce.mp3",
+            path: "./asset/music/Halo.mp3",
             image: "https://i1.wp.com/winnaijatv.com/wp-content/uploads/2021/08/Beyonce-Halo-DOWNLOAD-MP3.jpg?resize=708%2C708&ssl=1"
         },
         {
             name: 'Symphony',
             singer: 'Clean Bandit',
-            path: 'https://aredir.nixcdn.com/Sony_Audio37/Symphony-CleanBanditZaraLarsson-4822950.mp3?st=sPgJSXtRXYpT_rznXyez6g&e=1623144426',
+            path: './asset/music/Symphony.mp3',
             image: 'https://i.ytimg.com/vi/PIf9GvWaxQQ/maxresdefault.jpg',
         }, {
             name: 'Waiting For Love',
             singer: 'Avicii',
-            path: 'https://aredir.nixcdn.com/Unv_Audio45/WaitingForLove-Avicii-4203283.mp3?st=mXGv6kIqbxg_coAyUqzlnw&e=1623144462',
+            path: './asset/music/WaitingForLove.mp3',
             image: 'https://i.ytimg.com/vi/Hmbm3G-Q444/maxresdefault.jpg',
         }, {
             name: 'Alone',
             singer: 'Marshmello',
-            path: 'https://aredir.nixcdn.com/NhacCuaTui927/Alone-Marshmello-4456939.mp3?st=RTsMC9tNcKEi8fd0iKtdaA&e=1623144502',
+            path: './asset/music/Alone.mp3',
             image: 'https://i.ytimg.com/vi/UNB8F0ObA4g/maxresdefault.jpg',
         }, {
             name: 'Something Just Like This',
             singer: 'The Chainsmokers & Coldplay',
-            path: 'https://aredir.nixcdn.com/Sony_Audio39/SomethingJustLikeThis-TheChainsmokersColdplay-5337136.mp3?st=VQuH6VgNsPlBizbk-c7n3w&e=1623144556',
+            path: './asset/music/SomethingJustLikeThis.mp3',
             image: 'https://avatar-ex-swe.nixcdn.com/song/2017/11/07/a/1/4/5/1510038809679_640.jpg',
         }, {
             name: 'Sugar',
             singer: 'Maroon 5',
-            path: 'https://aredir.nixcdn.com/Unv_Audio73/Sugar-Maroon5-3338455.mp3?st=3FUWEyikJePPeAuREUcw9g&e=1623144644',
+            path: './asset/music/Sugar.mp3',
             image: 'https://i.ytimg.com/vi/7vw84EkHOlY/maxresdefault.jpg',
         },
 
